9fe3b0ab7d85fbcfea1cf1aa126c505ba4944322
zhaizzH <zhaizzh@qq.com>
2026-06-20 15:33:11 +0800

    来单提醒业务代码开发

---
+++ sky-server/src/main/java/com/sky/task/WebSocketTask.java
@@ -0,0 +1,14 @@
+package com.sky.task;
+
+import com.sky.websocket.WebSocketServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class WebSocketTask {
+    @Autowired
+    private WebSocketServer webSocketServer;
+}
